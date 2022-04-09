c3fc54a9892db4047bdf968c73209d9e6d574a96
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-09 22:31:46 +0100

    Create PostController web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [AppController::class, 'welcome']);
+
+Route::resource('/posts', PostController::class);
