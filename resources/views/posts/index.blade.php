30981fbed7897f3ee7574c467b7b645fcfd812c4
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-16 13:49:46 +0100

    add bootstrap

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -22,7 +24,7 @@
     </head>
     <body >
         {{-- show posts --}}
-        <div class="container">
+        <div class="container mt-4">
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
