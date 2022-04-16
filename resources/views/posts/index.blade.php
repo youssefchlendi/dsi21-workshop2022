375f9bb2825336c1228765977624fe355bf66474
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-16 14:49:55 +0100

    add navigation

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -23,7 +23,7 @@
         </style>
     </head>
     <body >
-        {{-- show posts --}}
+        @include('layouts.navigation')
         <div class="container mt-4">
             <div class="row">
                 <div class="col-md-12">
