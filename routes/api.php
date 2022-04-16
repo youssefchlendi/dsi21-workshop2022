7abaff6aa14c9cf59625a288ab0c540c9dd0fe5b
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-16 13:50:06 +0100

    set ressource to apiResource

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/posts', PostController::class);
+Route::apiResource('/posts', PostController::class);
